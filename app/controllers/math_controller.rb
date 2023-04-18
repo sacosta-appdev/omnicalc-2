0c6514b14e32892edffd2fa0db7ffce880de11d1
Santiago Acosta <sacostal@chicagobooth.edu>
2023-04-18 04:24:23 +0000

    Initial cleanup

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -6,4 +6,22 @@ class MathController < ApplicationController
 
   end
 
+  def subtract_form
+
+    render({ :template => "/math/subtract_form.html.erb"})
+
+  end
+
+  def multiply_form
+
+    render({ :template => "/math/multiply_form.html.erb"})
+
+  end
+
+  def divide_form
+
+    render({ :template => "/math/divide_form.html.erb"})
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/add", { :controller => "math", :action => "add_form"})
+  get("/subtract", { :controller => "math", :action => "subtract_form"})
+  get("/multiply", { :controller => "math", :action => "multiply_form"})
+  get("/divide", { :controller => "math", :action => "divide_form"})
 
 end
